5b0ba0b428482f5462acb18ca38bd56604dc0417
anupamdas104 <anupamdas104@gmail.com>
2019-03-06 18:26:52 -0500

    Merge with master

--- client/src/App.js
+++ client/src/App.js
@@ -1,42 +1,17 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-=======
 import React, { Component } from 'react';
 import Login from './components/Login';
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'
 import './App.css';
->>>>>>> 2a1323e34e91ddf034d76cc16d9162510209c761
 
 class App extends Component {
   render() {
     return (
-<<<<<<< HEAD
       <BrowserRouter>
         <div className="App">
           Occupied
         <Login />
         </div>
       </BrowserRouter>
-=======
-      <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.change
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header> */}
-      </div>
->>>>>>> 5be038710c6efd59922cd0e162bf80ff83d1905e
     );
   }
 }
