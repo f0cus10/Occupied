9c334ad4554e9974390ac5daf8dee45ba3874439
AceroM <miguel.acero81@myhunter.cuny.edu>
2019-03-11 18:36:08 -0400

    fix quick card.js thing

--- client/src/components/Grid.js
+++ client/src/components/Grid.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Grid, Image } from "semantic-ui-react";
 import data from "../dummydata.json";
-import CardExampleCard from "./Card.js";
+import CardExampleCard from "./Card";
 
 class BlueprintGrid extends Component {
   constructor(props) {
