d130e13c9d7d7429742874c6a145b4003c02a7b5
anupamdas104 <anupamdas104@gmail.com>
2019-03-25 17:40:25 -0400

    Added create blueprint user interface

--- client/src/components/CreateBlueprint.js
+++ client/src/components/CreateBlueprint.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
+import { Button, Form } from 'semantic-ui-react'
 
 class CreateBlueprint extends Component {
   render() {
@@ -9,6 +10,18 @@ class CreateBlueprint extends Component {
       <div>
         <h1>Create Blueprint</h1>
         <h2>Welcome {username} !</h2>
+        <Form>
+        <Form.Field>
+          <label className="label"> Building Name</label>
+          <input placeholder='Building Name'/>
+        </Form.Field>
+        <Form.Field>
+          <label>Address</label>
+          <input placeholder='Address' />
+        </Form.Field>
+        <Form.TextArea label='Description' placeholder='Add description...' />
+        <Button type='submit'>Submit</Button>
+        </Form>
         <Link to="/home">GO TO HOME</Link>
       </div>
     );
