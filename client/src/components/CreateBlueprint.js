52db6e05030482baff312b57e5fec191f5310eef
AceroM <miguel.acero81@myhunter.cuny.edu>
2019-04-08 18:31:25 -0400

    Added post request to create blueprint

--- client/src/components/CreateBlueprint.js
+++ client/src/components/CreateBlueprint.js
@@ -4,22 +4,44 @@ import { Link } from "react-router-dom";
 import { Button, Form } from 'semantic-ui-react'
 
 class CreateBlueprint extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      description: ''
+    }
+  }
+
+  createBlueprint = (name, description) => {
+    let data = { name, description, isPublic: true, userId: 1, blueprintId: 10, username: 'miguel', category: 'House'};
+    fetch('/api/blueprint/create', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json",
+        // "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: JSON.stringify(data)
+    })
+    .then(res => console.log(res))
+  }
+
   render() {
     const { username } = this.props;
+    const { name, description } = this.state;
     return (
       <div>
         <h1>Create Blueprint</h1>
         <h2>Welcome {username} !</h2>
-        <Form>
+        <Form onSubmit={() => this.createBlueprint(name, description)}>
         <Form.Field>
           <label className="label"> Building Name</label>
-          <input placeholder='Building Name'/>
+          <input onChange={(e) => this.setState({name: e.target.value})} placeholder='Building Name'/>
         </Form.Field>
         <Form.Field>
           <label>Address</label>
           <input placeholder='Address' />
         </Form.Field>
-        <Form.TextArea label='Description' placeholder='Add description...' />
+        <Form.TextArea label='Description' onChange={(e) => this.setState({description: e.target.value})} placeholder='Add description...' />
         <Button type='submit'>Submit</Button>
         </Form>
         <Link to="/home">GO TO HOME</Link>
