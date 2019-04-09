4a8c72b6c819684306f1f150e4170dffcc806ca0
Iftikhar Khan <iakhan4425@gmail.com>
2019-04-09 15:23:13 -0400

    Add protection to user, space, and blueprint routes

--- api/index.js
+++ api/index.js
@@ -1,11 +1,36 @@
 const router = require('express').Router();
+const ProtectedRoutes = require('express').Router();
+const jwt = require('jsonwebtoken');
 const dummyUsers = require('../dummy/users.json');
 const dummyBlueprints = require('../dummy/blueprints.json');
 const { User, Blueprint, Space } = require('../models');
 
-router.use('/user', require('./user'));
-router.use('/blueprint', require('./blueprint'));
-router.use('/space', require('./space'));
+
+ProtectedRoutes.use((req, res, next) => {
+  //check header for the token
+  var token = req.headers['access-token'];
+  console.log(`The token provided was: ` + token);
+  if(token){
+    jwt.verify(token, require('./config').secret, (err, decoded) => {
+      if (err){
+        return res.json({ message: 'Invalid Token' });
+      }
+      else{
+        req.decoded = decoded;
+        next();
+      }
+    }) 
+  }
+  else {
+    res.send({
+      message: "No token provided"
+    })
+  }
+});
+
+router.use('/user', ProtectedRoutes, require('./user'));
+router.use('/blueprint', ProtectedRoutes, require('./blueprint'));
+router.use('/space', ProtectedRoutes, require('./space'));
 router.use(require('./authenticate'));
 
 router.get('/populate', async (req, res, next) => {
