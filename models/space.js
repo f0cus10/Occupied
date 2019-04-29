00be186e5e249b9b3e16300ce6babbc7b76bc830
MarjanGit <mansar16@huntersoe.org>
2019-04-29 17:40:53 -0400

    sort by

--- models/space.js
+++ models/space.js
@@ -1,38 +1,48 @@
 module.exports = (sequelize, DataTypes) => {
-  const Space = sequelize.define('space', {
-    name: {
-      type: DataTypes.STRING,
-      validate: {
-        notEmpty: true,
-        // allowNull: false,
+  const Space = sequelize.define(
+    "space",
+    {
+      name: {
+        type: DataTypes.STRING,
+        validate: {
+          notEmpty: true
+          // allowNull: false,
+        }
+      },
+
+      description: {
+        type: DataTypes.STRING,
+        defaultValue: ""
+      },
+
+      category: {
+        type: DataTypes.STRING,
+        defaultValue: ""
+      },
+
+      occupied: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      },
+
+      imageUrl: {
+        type: DataTypes.STRING,
+        defaultValue: ""
       }
     },
-
-    description: {
-      type: DataTypes.STRING,
-    },
-
-    occupied: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: false,
-    },
-
-    imageUrl: {
-      type: DataTypes.STRING,
-      defaultValue: ""
+    {
+      timestamps: false
     }
-  }, {
-    timestamps: false
-  });
+  );
 
-  Space.associate = (models) => {
+  Space.associate = models => {
     // space can only belong to one blueprint through blueprintId
     Space.belongsTo(models.Blueprint, {
-      foreignKey: 'blueprintId',
+      foreignKey: "blueprintId"
     });
     // current user that occupies space
     Space.belongsTo(models.User);
   };
 
   return Space;
-}
+};
