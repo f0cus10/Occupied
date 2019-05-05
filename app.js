fc615f34ee792664230e28a4858f17fd6b4b2100
AceroM <miguel.acero81@myhunter.cuny.edu>
2019-05-04 22:36:35 -0400

    Revert "heroku stuff..."
    This reverts commit 9944169adf9499bba3bd4b1c6458861f88b5d0d6.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
 const db = require('./models');
 const { User, Blueprint, Space } = require('./models');
@@ -12,17 +11,11 @@ const app = express();
 db.sequelize.sync({ force: true }).then(() => console.log('synced!'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use('/api', require('./api'));
 
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    });
-}
+app.get('/', (req, res) => {
+    res.send(process.env.PGUSER);
+});
 
 //populates users and creates associations
 (async () => {
