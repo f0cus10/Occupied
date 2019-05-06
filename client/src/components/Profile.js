a48677593005dbd9e9d539103818c0a4dbac3261
AceroM <miguel.acero81@myhunter.cuny.edu>
2019-05-06 17:09:34 -0400

    changed profile page

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import '../styles/Profile.css';
 import Cookies from 'js-cookie';
 import axios from 'axios';
-
+import { DataTable, Label, Page, CalloutCard, Card, DescriptionList } from '@shopify/polaris';
 
 class Profile extends Component {
   constructor(props) {
@@ -26,35 +26,62 @@ class Profile extends Component {
   render(){
     // const { data, profileId } = this.props;
     const { data } = this.state;
-    let username, email, description, imageUrl;
+    let username, email, description, imageUrl, blueprints, rows = [['','','','']];
     if (data) {
       username = data.username;
       email = data.email;
+      blueprints = data.blueprints;
       description = data.description;
       imageUrl = data.imageUrl;
     }
+    if (blueprints) {
+      rows = blueprints.map(b => ([b.name, b.category, String(b.isPublic), b.description]))
+    }
     
     return (
-      <div >
-        <div className="main">
-          <div className="card">
-            <img className="picture"
-              src={imageUrl}
+      <Page title="Profile">
+        <Card>
+          <CalloutCard
+            title={username}
+            illustration={imageUrl}
+            primaryAction={{
+              content: "Add Friend",
+              url: "https://www.shopify.com"
+            }}
+          >
+            <DescriptionList
+              items={[
+                {
+                  term: "Email",
+                  description: email
+                },
+                {
+                  term: "Description",
+                  description: description
+                }
+              ]}
             />
-          </div>
-          <div className="card">
-            <div>
-              Username: {username} 
-            </div>
-            <div>
-              Email: {email}
-            </div>
-            <div>
-              Description: {description}
-            </div>
-          </div>
-        </div>
-      </div>
+          </CalloutCard>
+          <Label> Blueprints </Label> 
+          <Card>
+            <DataTable
+              columnContentTypes={[
+                "text",
+                "text",
+                "text",
+                "text",
+              ]}
+              headings={[
+                "Name",
+                "Category",
+                "Public",
+                "Description",
+              ]}
+              rows={rows}
+            />
+          </Card>
+        </Card>
+      </Page>
     );
   }
 }
